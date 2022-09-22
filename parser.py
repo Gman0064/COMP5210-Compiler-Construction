4a0afd8d46437a0214dfdb36c7270020d6aa2cbe
Yuantai Pan <tiger.stevesyd.pan@gmail.com>
2022-09-22 03:42:37 -0500

    Finished parser v1, ready for testing

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@
 from typing import Any
 import json
 
+
 class Parser:
 
     def __init__(self):
@@ -12,5 +13,31 @@ class Parser:
 
         self.config = json.loads(f_data)
 
+        f2 = open("config/grammarV1", "r")
+        f2_data = f2.read()
+        f2.close()
+
+        self.grammar = json.loads(f2_data)
+
     def parse(self, token_list: list):
-        return None
+        output = []
+        output.push("invalid")
+        output.push("0")
+        token = token_list.popleft()
+        while True:
+            state = output.top()
+            if self.config["Action"][state][token]["action"] == "shift":
+                output.push(token)
+                output.push(self.config["Action"][state][token]["num"])
+                token = token_list.popleft()
+            elif self.config["Action"][state][token]["action"] == "reduce":
+                for _ in num((self.grammar[self.config["Action"][state][token][num]].size() - 1) * 2):
+                    output.pop()
+                state = output.top()
+                reduction = self.grammar[self.config["Action"][state][token][num]][0]
+                output.push(reduction)
+                output.push(self.config[state][reduction])
+            elif self.config["Action"][stete][token]["action"] == "acc":
+                return output
+            else:
+                return 0
